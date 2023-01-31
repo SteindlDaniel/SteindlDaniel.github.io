48e82244d82a9035b5e6147ade09406389296989
SteindlDaniel <94899375+SteindlDaniel@users.noreply.github.com>
2023-01-31 19:43:12 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/f/mwkjpvzlAAAABBBBCCCCDDDDASD",
+                url: "https://formspree.io/f/asdfghjklmwkjpvzlASDFGHJKL",
                 type: "POST",                
                 headers: {
                     'Accept': 'application/json'
